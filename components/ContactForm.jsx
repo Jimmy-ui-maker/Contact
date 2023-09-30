9b99c15f21ae50162d2eaf9067a197206ca7c8ec
Jimmy Yakubu <jimiyaks3@gmail.com>
2023-09-30 15:58:21 -0700

    fixed erro

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -45,7 +45,7 @@ export default function ContactForm() {
         onSubmit={handleSubmit}
         className="py-4 mt-4 border-t flex flex-col gap-5"
       >
-        <div>
+        <div >
           <label htmlFor="fullname">Full Name</label>
           <input
             onChange={(e) => setFullname(e.target.value)}
@@ -85,7 +85,7 @@ export default function ContactForm() {
 
       <div className="bg-slate-100 flex flex-col">
         {error && error.map((e) => (
-          <div className={`${success ? "text-green-800" : "text-red-600"} px-5 py-2`}>{e}</div>
+          <div key={e.id} className={`${success ? "text-green-800" : "text-red-600"} px-5 py-2`}>{e}</div>
         ))}
       </div>
     </>
